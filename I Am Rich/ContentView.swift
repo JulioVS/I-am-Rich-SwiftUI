a0fa305a9dfa084661867eea69a1e8a145468c5e
JulioVS <julioer@me.com>
2026-01-28 14:54:37 -0300

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Julio Errecart on 28/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Julio Errecart on 28/1/26.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
